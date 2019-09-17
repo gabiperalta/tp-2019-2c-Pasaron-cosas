c0a27b494cc7156924cacd550c39ec328f1788ab
gabiperalta <gabiperalta16398@gmail.com>
2019-09-16 22:52:06 -0300

    Agregado .gitignore en biblioteca

--- biblioteca/biblioteca_sockets.h
+++ biblioteca/biblioteca_sockets.h
@@ -12,7 +12,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include "biblioteca.h"
+//#include "biblioteca.h"
 
 
 #define PUERTO_ESCUCHA_FS 8090
@@ -20,16 +20,16 @@
 #define IP_LOCAL "127.0.0.1"
 
 
-int escuchar(int puerto);
-int conectarseA(char *ip, int puerto);
-int aceptarConexion(int socketEscucha);
-int enviarMensaje(char* mensaje, int socketDestino);
-char* recibirMensaje(int socketCliente);
-void enviarRequest(int servidor, t_request request);
-void enviarResponse(int cliente,t_response response);
-void enviarCantidadDeDescribes(int cliente,uint8_t cantidadDeDescribes);
-t_request recibirRequest(int servidor);
-t_response recibirResponse(int servidor);
+//int escuchar(int puerto);
+//int conectarseA(char *ip, int puerto);
+//int aceptarConexion(int socketEscucha);
+//int enviarMensaje(char* mensaje, int socketDestino);
+//char* recibirMensaje(int socketCliente);
+//void enviarRequest(int servidor, t_request request);
+//void enviarResponse(int cliente,t_response response);
+//void enviarCantidadDeDescribes(int cliente,uint8_t cantidadDeDescribes);
+//t_request recibirRequest(int servidor);
+//t_response recibirResponse(int servidor);
 
 
 #endif
