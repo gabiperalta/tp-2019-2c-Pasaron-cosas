de65b76c66288e67f49a1bda683d0ea9c80da715
giuliposteraro <giuliposteraro@hotmail.com>
2019-09-25 20:15:01 -0300

    algunos avances con las colas

--- suse/src/suse.c
+++ suse/src/suse.c
@@ -2,5 +2,35 @@
 #include <stdlib.h>
 #include <suse.h>
 
+void inicializarColaNew(){
+	//llegan los hilos con hilolay_create
+		char* hilo = malloc(100);
+		cola_new = list_create();
+		for(int i = 0; i < cola_new->elements_count; i++){
+			list_add(cola_new, hilo);
+		}
 
+		free(hilo);
+		return;
+}
+void inicializarColaReady(){
+	cola_ready= list_create();
 
+}
+void pasarDeNewAReady(){
+	int cantidad_max_procesos = obtenerGradoMultiprogramacion();
+	for(int i = 0; i < cantidad_max_procesos; i++){
+
+	}
+}
+
+int obtenerGradoMultiprogramacion(){
+	t_config * config = obtenerConfigDeSuse();
+	int grado_multiprog = config_get_int_value(config,"MAX_MULTIPROG");
+	return grado_multiprog;
+}
+
+t_config * obtenerConfigDeSuse(){
+	t_config * config = config_create("/home/utnso/tp-2019-2c-Pasaron-cosas/suse/src/suse.config");
+	return config;
+}
--- suse/src/suse.h
+++ suse/src/suse.h
@@ -10,8 +10,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <inttypes.h>
 #include <commons/collections/dictionary.h>
+#include <commons/collections/list.h>
+#include <commons/config.h>
+#include <semaphore.h>
 
+                       /* Estructuras*/
+t_list * cola_new;
+t_list * cola_ready;
+t_list* cola_exit;
+t_list * cola_blocked;
 
+typedef struct{
+	int cant_instancias_disponibles;
+	t_list * hilos_bloqueados;
+}semaforos_suse;
+                       /* Funciones */
+void inicializarColaNew();
+void inicializarColaReady();
+void pasarDeNewAReady();
+int obtenerGradoMultiprogramacion();
+t_config * obtenerConfigDeSuse();
 #endif /* SUSE_H_ */
