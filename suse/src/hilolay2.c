101d323903a992a2294fcaca30f50173b8ff9746
giuliposteraro <giuliposteraro@hotmail.com>
2019-10-06 21:10:52 -0300

    agregue dos archivos que nos van a servir para simular hilolay y crear sus funciones adentro

---
+++ suse/src/hilolay2.c
@@ -0,0 +1,8 @@
+/*
+ * hilolay2.c
+ *
+ *  Created on: 6 oct. 2019
+ *      Author: utnso
+ */
+
+
---
+++ suse/src/hilolay2.h
@@ -0,0 +1,13 @@
+/*
+ * hilolay.h
+ *
+ *  Created on: 6 oct. 2019
+ *      Author: utnso
+ */
+
+#ifndef HILOLAY2_H_
+#define HILOLAY2_H_
+
+
+
+#endif /* HILOLAY2_H_ */
