0664cef821305618c8c10bbc401b83a027cf5e32
carolinacaampos <caro_campos98@hotmail.com>
2019-11-12 19:21:05 -0300

    modelo scoket

--- suse/src/funcionesSuse.c
+++ suse/src/funcionesSuse.c
@@ -7,6 +7,101 @@
 
 #include <funcionesSuse.h>
 
+void procesar_solicitud(void* socket_cliente){
+	t_paquete paquete = recibir_paquete(socket_cliente);
+	void (*funcion_suse)(t_paquete,int);
+
+	// por cada conexion nueva, agrego al proceso/hilo a la lista de threads
+
+	while(paquete.error != 1){
+		switch(paquete.header){
+			case SUSE_CREATE:
+				funcion_suse = funcion_init;
+				break;
+			case SUSE_SCHEDULE_NEXT:
+				funcion_suse = funcion_schedule_next;
+				break;
+			case SUSE_WAIT:
+				funcion_suse = funcion_wait;
+				break;
+			case SUSE_SIGNAL:
+				funcion_suse = funcion_signal;
+				break;
+			case SUSE_JOIN:
+				funcion_suse = funcion_join;
+				break;
+			case SUSE_CLOSE:
+				funcion_suse = funcion_close;
+				break
+			//	return;
+		}
+
+		funcion_suse(paquete,socket_cliente);
+
+		paquete = recibir_paquete(socket_cliente);
+	}
+
+	close(socket_cliente);
+}
+
+void funcion_init(t_paquete paquete,int socket_muse){
+
+/*	char* id_programa = string_new();
+	char* ip_socket = obtener_ip_socket(socket_muse);
+	string_append(&id_programa,string_itoa(obtener_valor(paquete.parametros)));
+	string_append(&id_programa,"-");
+	string_append(&id_programa,ip_socket);
+
+	list_add(lista_procesos,crear_proceso(id_programa,socket_muse));
+
+	//				PRUEBA
+	/*
+	t_thread* thread_obtenido;
+	for(int i=0; i<list_size(lista_threads); i++){
+		thread_obtenido = list_get(lista_threads,i);
+		printf("hilo nro %d\t",i);
+		printf("id_programa: %s\t",thread_obtenido->id_programa);
+		printf("socket: %d\t\n",thread_obtenido->socket);
+	}
+	printf("\n");
+
+
+	free(ip_socket); // Sacar si falla
+	*/
+
+}
+
+
+
+
+char* obtener_ip_socket(int s){
+	socklen_t len;
+	struct sockaddr_storage addr;
+	char ipstr[INET6_ADDRSTRLEN];
+	char* ipstr_reservado = malloc(sizeof(ipstr));
+	//int port;
+
+	len = sizeof addr;
+	getpeername(s, (struct sockaddr*)&addr, &len);
+
+	// deal with both IPv4 and IPv6:
+	if (addr.ss_family == AF_INET) {
+	    struct sockaddr_in *s = (struct sockaddr_in *)&addr;
+	    //port = ntohs(s->sin_port);
+	    inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
+	} else { // AF_INET6
+	    struct sockaddr_in6 *s = (struct sockaddr_in6 *)&addr;
+	    //port = ntohs(s->sin6_port);
+	    inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
+	}
+
+	strcpy(ipstr_reservado,ipstr);
+
+	return ipstr_reservado;
+}
+
+
+
 
 void servidor(){
 	void * conectado;
@@ -21,34 +116,6 @@ void servidor(){
 	}
 }
 
-void procesar_solicitud(void* socket_cliente){
-	t_paquete paquete = recibir_paquete(socket_cliente);
-	void (*funcion_suse)(t_paquete,int);
-
-	while(paquete.error != 1){
-		switch(paquete.header){
-			case SUSE_INIT;
-				break;
-			case SUSE_CREATE;
-				break;
-			case SUSE_SCHEDULE_NEXT;
-				break;
-			case SUSE_WAIT;
-				break;
-			case SUSE_SIGNAL;
-				break;
-			case SUSE_JOIN;
-				break;
-			case SUSE_CLOSE;
-				break;
-
-				return;
-		}
-
-		//funcion_muse(paquete,socket_cliente);
-
-		paquete = recibir_paquete(socket_cliente);
-	}
 
 void iniciarPlanificacion(){
 	pthread_t hilo;
