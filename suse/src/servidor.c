6cdcf5a1e8629ce44d550e7a5740ae907673e650
carolinacaampos <caro_campos98@hotmail.com>
2019-11-21 16:40:12 -0300

    retorno suse_create

--- suse/src/servidor.c
+++ suse/src/servidor.c
@@ -56,7 +56,7 @@ void funcion_create(t_paquete paquete,int socket_suse){
 
 
 	//aca le das la orden a suse
-	planificate(tid)//funcion suse)(tid); //podria tener una respuesta
+	retorno = planificate(tid)//funcion suse)(tid); //podria tener una respuesta
 
 	//en caso que tenga retorno int retorno = crearArchivo( path );
 
