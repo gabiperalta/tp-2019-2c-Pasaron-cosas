8e232669b17c7c7625310d1c1840a2e91ef9f984
gabiperalta <gabiperalta16398@gmail.com>
2019-12-01 17:57:07 -0300

    Agrego archivos en pruebas

---
+++ pruebas/planificacion/prueba_planificacion.sh
@@ -0,0 +1,30 @@
+cd tp-2019-2c-Pasaron-cosas/biblioteca/Debug/
+make clean
+make
+cd
+
+cd tp-2019-2c-Pasaron-cosas/biblioteca/Debug/
+sudo cp libbiblioteca.so /usr/lib/
+cd
+
+#cd tp-2019-2c-Pasaron-cosas/suse/Debug/
+#make clean
+#make
+#cd
+
+cd tp-2019-2c-Pasaron-cosas/LibMuse/Debug/
+make clean
+make
+cd
+
+cd tp-2019-2c-Pasaron-cosas/LibMuse/Debug/
+sudo cp liblibmuse.so /usr/lib/
+cd
+
+cd tp-2019-2c-Pasaron-cosas/muse/Debug/
+make clean
+make
+cd
+
+cp tp-2019-2c-Pasaron-cosas/pruebas/memoria/suse.config tp-2019-2c-Pasaron-cosas/suse/src/
+cp tp-2019-2c-Pasaron-cosas/pruebas/memoria/muse.config tp-2019-2c-Pasaron-cosas/muse/
